cce2e1e85a8fccd6ab40e0126be0221292ed63b0
Austin_Bratcher <arb.bratcher@gmail.com>
2015-05-25 16:45:21 -0500

    new routes

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,53 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/**
+ * Route to home page
+ */
 router.get('/', function(req, res, next) {
   // TODO pull title from database under certain
   res.render('body/index', { title: 'FBC Waco College' });
 });
 
+/**
+ * Route to contact page
+ */
 router.get('/contact', function(req, res, next) {
     // TODO pull title from database under certain
-    res.render('body/contact', { title: 'FBC Waco College' });
+    res.render('body/basic_pages/contact', { title: 'Contact Us' });
 });
+
+/**
+ * Route to staff page
+ */
+router.get('/staff', function(req, res, next) {
+    // TODO pull title from database under certain
+    res.render('body/basic_pages/staff', { title: 'Staff' });
+});
+
+/**
+ * Route to Student Leader page
+ */
+router.get('/leaders', function(req, res, next) {
+    // TODO pull title from database under certain
+    res.render('body/basic_pages/leaders', { title: 'Leaders' });
+});
+
+/**
+ * Route to Gathers page
+ */
+router.get('/gatherings', function(req, res, next) {
+    // TODO pull title from database under certain
+    res.render('body/basic_pages/gatherings', { title: 'Gatherings' });
+});
+
+/**
+ * Route to Beliefs page
+ */
+router.get('/beliefs', function(req, res, next) {
+    // TODO pull title from database under certain
+    res.render('body/basic_pages/beliefs', { title: 'Beliefs' });
+});
+
 
 module.exports = router;
